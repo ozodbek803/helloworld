5edbbb69bf7b04697af81eb56d602e003e0eb1d7
ozodbekyarashev12 <ozodbekyarashev.info@gmail.com>
2025-02-13 13:37:30 +0500

    h

---
+++ week 5.cpp
@@ -0,0 +1,140 @@
+#include <iostream>
+using namespace std;
+int main(){
+  // problem 1.1
+  // for (int i = 1; i <= 10; i++){
+  //   cout << i<<" " ;
+  // }
+
+  //problem 1.2
+  // int num;
+  // cout << "Enter a number: ";
+  // cin >> num;
+  // for (int i=1; i<=num;i++) {
+  //   cout << i<<" ";
+  // }
+
+  //problem 1.3
+  // int num=10,sum;
+  // sum=0;
+  // for (int i=0;i<num;i++) {
+  //   sum+=i;
+  // }
+  // cout<<sum;
+
+  //problem 1.4
+  // int num,sum;
+  // cout<<"Enter a number:";
+  // cin>>num;
+  // sum=0;
+  // for (int i=1;i<=num;i++) {
+  //   sum+=i;
+  // }
+  // cout<<"The sum is: "<<sum<<endl;
+
+  //problem 1.5
+   // int num=10,sum;
+   // sum=0;
+   // for(int i=1;i<=num;i++) {
+   //     sum+=i;
+   // }
+   //  double average=sum/num;
+   //  cout<<average<<endl;
+
+  //problem 1.6
+    // double num,sum;
+    // cout<<"Enter a number:";
+    // cin>>num;
+    // sum=0;
+    // for (int i=1;i<=num;i++) {
+    //     sum+=i;
+    // }
+    // double average=sum/num;
+    // cout<<"Average:"<<average<<endl;
+
+    //problem 2.1
+    // for (int i = 1; i <= 10; i=i+2) {
+    //     cout << i <<";
+    // }
+
+    //problem 2.2
+    // for (int i = 2; i <= 10; i=i+2) {
+    //     cout << i << " ";
+    // }
+
+    //problem 2.3
+    // int num;
+    // cout << "Enter a number: ";
+    // cin >> num;
+    // for (int i=1;i<=num;i=i+2) {
+    //     cout <<i<<" ";
+    // }
+
+    //problem 2.4
+    // int num;
+    // cout<<"Enter a number: ";
+    // cin>>num;
+    // for (int i=2;i<=num;i=i+2) {
+    //     cout<<i<<" ";
+    // }
+
+    //problem 2.5
+    // for(int i=10;i<=100;i=i+10) {
+    //     cout<<i<<" ";
+    // }
+
+    // problem 2.6
+    // int num;
+    // cout<<"Enter a number: ";
+    // cin>>num;
+    // for (int i = 10; i <= num; i=i+10) {
+    //     cout<<i<<" ";
+    // }
+
+    //problem 3
+    // for (int i=65; i<=90; i++) {
+    //     cout << static_cast<char>(i) << " ";
+    //     if ((i-64)%5==0) {
+    //         cout << endl;
+    //     }
+    // }
+
+    //problem 4
+    // int num,factorial;
+    // factorial=1;
+    // cout<<"Enter a number: ";
+    // cin>>num;
+    // if(num>0) {
+    //     for (int i = 1; i <= num; i++ ) {
+    //         factorial *=i;
+    //     }
+    //     cout<<"Factorial of the given number is: "<<factorial;
+    // }
+    // else{
+     //     cout<<"Factorial of a negative number is not defined";
+     // }
+
+    //problem 5
+    int classes,credits,total;
+    cout<<"Enter number of classes:";
+    cin>>classes;
+    for (int i=0;i<classes;i++) {
+        cout<<"Enter number of credits:";
+        cin>>credits;
+        for (int j=0;j<credits
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+  return 0;
+}
