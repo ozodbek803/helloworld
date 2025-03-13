e0f5ca95bffa7b16367be051201985f9660e42ed
ozodbek803 <o.yarashev@newuu.uz>
2025-03-13 14:55:20 +0500

    Add files via upload

---
+++ week9.cpp
@@ -0,0 +1,120 @@
+#include <iostream>
+using namespace std;
+//problem 1
+// int multiply(int a, int b) {
+//     if (a==0 ||b==0) {
+//         return 0;
+//     }
+//     return a+multiply(a,b-1);
+//
+// }
+//
+// int main () {
+//     int num1 , num2;
+//     cout<<"Enter numbers: ";
+//     cin>>num1>> num2;
+//     cout<<multiply(num1,num2)<<endl;
+//
+// }
+
+//problem 2
+// int factorial(int num) {
+//     if (num==1 || num==0) {
+//         return 1;
+//     }
+//     return num*factorial(num-1);
+// }
+// int main() {
+//     int number;
+//     cout<<"Please enter a number: ";
+//     cin>>number;
+//     cout<<factorial(number)<<endl;
+// }
+
+// problem 3
+// int fibonacci(int n) {
+//     if (n==0) {
+//         return 0;
+//     }
+//     if (n==1) {
+//         return 1;
+//     }
+//     return fibonacci(n-1)+fibonacci(n-2);
+// }
+// int main() {
+//     int num;
+//     cout<<"Enter a number: ";
+//     cin>>num;
+//     cout<<fibonacci(num);
+// }
+
+//problem 4
+// int sum(int n) {
+//     if (n==0) return 0;
+//     return n+sum(n-1);
+// }
+//
+// int main() {
+//     int num;
+//     cout<<"Please enter a number: ";
+//     cin>>num;
+//     cout<<sum(num)<<endl;
+// }
+
+//problem 5
+// int descend(int n) {
+//     if (n==1) return 1;
+//     cout<< n<<" ";
+//     return descend(n-1);
+// }
+// int main() {
+//     int num;
+//     cout<<"Please enter a number: ";
+//     cin>>num;
+//     cout<<descend(num)<<endl;
+// }
+
+//problem 6
+// int count(int n) {
+//     if (n==0) return 0;
+//     return 1+count(n/10);
+// }
+// int main() {
+//     int num;
+//     cout<<"Please enter a number: ";
+//     cin>>num;
+//     cout<<count(num);
+// }
+
+//problem 7
+// int sum(int n) {
+//     if (n==0) return 0;
+//     return n%10+sum(n/10);
+//     }
+// int main() {
+//     int num;
+//     cout<<"Please enter a number: ";
+//     cin>>num;
+//     cout<<sum(num);
+// }
+
+//problem 8
+// int power(int base, int exponent) {
+//     if (exponent == 0) return 1;
+//     if (exponent == 1) return base;
+//     return base*power(base, exponent-1);
+// }
+// int main() {
+//     int a,b;
+//     cout<<"Enter the base: ";
+//     cin>>a;
+//     cout<<"Enter the exponent: ";
+//     cin>>b;
+//     cout <<a<<"^"<<b<<"="<<power(a,b)<<endl;
+//
+//
+// }
+
+//problem 9
+
+
